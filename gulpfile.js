818659928e28e789d2045fbe27ae0ecf124578ea
Craig McLellen <cmclellen@gmail.com>
2015-07-25 14:59:03 +1000

    Hooked up index.html and js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,40 @@
 var gulp = require('gulp'),
-  babel = require('gulp-babel');
+  babel = require('gulp-babel'),
+  concat = require('gulp-concat'),
+  sourcemaps = require('gulp-sourcemaps'),
+  browserSync = require('browser-sync').create();
+
+var paths = {
+  dist: './dist',
+  app: './app'
+};
 
 gulp.task('js', function() {
-  return gulp.src('src/app.js')
+  return gulp.src(paths.app + '/**/*.js')
+    .pipe(sourcemaps.init())
     .pipe(babel())
-    .pipe(gulp.dest('dist'));
+    .pipe(concat('all.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.dist + '/js'));
 });
 
-gulp.task('default', ['js']);
+gulp.task('html', function() {
+  return gulp.src(paths.app + '/**/*.html')
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: paths.dist
+        }
+    });
+});
+
+gulp.task('watch', function() {
+
+  gulp.watch(paths.app + "/*.html").on('change', browserSync.reload);
+
+});
+
+gulp.task('default', ['html', 'js', 'watch', 'serve']);
