78a318e34894551d6be99aed37848d54ef43728c
Craig McLellen <cmclellen@gmail.com>
2015-07-25 15:17:58 +1000

    Added favicon

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,22 @@ var gulp = require('gulp'),
 
 var paths = {
   dist: './dist',
-  app: './app'
+  app: './app',
+  bower_components: './bower_components'
 };
 
+gulp.task('css', function() {
+
+  var files = [
+    paths.bower_components+'/bootstrap/dist/css/bootstrap.css'
+  ];
+
+  return gulp.src(files)
+    .pipe(gulp.dest(paths.dist + '/css'));
+});
+
 gulp.task('js', function() {
-  return gulp.src(paths.app + '/**/*.js')
+  return gulp.src(paths.app + '/js/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(concat('all.js'))
@@ -19,22 +30,20 @@ gulp.task('js', function() {
 });
 
 gulp.task('html', function() {
-  return gulp.src(paths.app + '/**/*.html')
+  return gulp.src([paths.app + '/**/*.html', paths.app + '/*.ico'])
     .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('serve', function() {
-    browserSync.init({
-        server: {
-            baseDir: paths.dist
-        }
-    });
+  browserSync.init({
+    server: {
+      baseDir: paths.dist
+    }
+  });
+
+  gulp.watch(paths.app + "/css/**/*.css", ['js']).on('change', browserSync.reload);  
+  gulp.watch(paths.app + "/js/**/*.js", ['js']).on('change', browserSync.reload);  
+  gulp.watch(paths.app + "/*.html", ['html']).on('change', browserSync.reload);
 });
 
-gulp.task('watch', function() {
-
-  gulp.watch(paths.app + "/*.html").on('change', browserSync.reload);
-
-});
-
-gulp.task('default', ['html', 'js', 'watch', 'serve']);
+gulp.task('default', ['html', 'js', 'css', 'serve']);
